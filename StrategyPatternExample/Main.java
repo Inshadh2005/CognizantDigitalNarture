1ddc9b3e5f72eed4f5c6e4a3f200c6febf992a7e
Inshadh2005 <shaikmohammedinshadh@gmail.com>
2026-06-24 22:32:01 +0530

    Delete StrategyPatternExample directory

--- StrategyPatternExample/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        PaymentContext payment =
-                new PaymentContext(new CreditCardPayment());
-
-        payment.executePayment(1000);
-    }
-}
