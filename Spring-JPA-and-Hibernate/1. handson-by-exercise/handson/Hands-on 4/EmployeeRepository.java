36c9df5fda67029e89c1a3578d9d594be9d9860c
Inshadh2005 <shaikmohammedinshadh@gmail.com>
2026-07-06 17:35:51 +0530

    Delete Spring-JPA-and-Hibernate directory

--- Spring-JPA-and-Hibernate/1. handson-by-exercise/handson/Hands-on 4/EmployeeRepository.java
+++
@@ -1,3 +0,0 @@
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
--- Spring-JPA-and-Hibernate/1. handson-by-exercise/handson/Hands-on 4/EmployeeService.java
+++
@@ -1,7 +0,0 @@
-@Autowire
-private EmployeeRepository employeeRepository;
-
-@Transactional
-public void addEmployee(Employee employee) {
-    employeeRepository.save(employee);
-}
